4f29f996fbf57bde37b78f65481584c2cb4c030b
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-18 06:17:12 +0200

    Add paddle and ball moves

--- pongy.py
+++ pongy.py
@@ -7,26 +7,81 @@ window.setup(width=800, height=600)
 # prevent window from updating
 window.tracer(0)
 
-# Paddle A - Yellow Turtle
+# Paddle A - yellow square
 paddle_a = t.Turtle()
 paddle_a.speed(0)
-paddle_a.shape("turtle")
+paddle_a.shape("square")
 paddle_a.color("yellow")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
 
-# Paddle B - Blue Square
-paddle_a = t.Turtle()
-paddle_a.speed(0)
-paddle_a.shape("square")
-paddle_a.color("blue")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
-paddle_a.penup()
-paddle_a.goto(350, 0)
+# Paddle B - blue square
+paddle_b = t.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("blue")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
 
 # Ball
+ball = t.Turtle()
+ball.speed(0)
+ball.shape("turtle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 0.25
+ball.dy = -0.25
+
+
+# Moving
+def paddle_a_up():
+    y = paddle_a.ycor()
+    y += 20
+    paddle_a.sety(y)
+
+
+def paddle_a_down():
+    y = paddle_a.ycor()
+    y -= 20
+    paddle_a.sety(y)
+
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
+# Keyboard binding
+window.listen()
+window.onkeypress(paddle_a_up, "w")
+window.onkeypress(paddle_a_down, "s")
+window.onkeypress(paddle_b_up, "Up")
+window.onkeypress(paddle_b_down, "Down")
 
 # Main Game Loop
 while True:
     window.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
