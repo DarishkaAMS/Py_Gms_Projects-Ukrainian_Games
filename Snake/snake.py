e3f1b046193d318f04f765ffa9b56042158150da
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-19 07:33:16 +0200

    Add Cube and Snake

---
+++ Snake/snake.py
@@ -0,0 +1,34 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+
+class Cube(object):
+    def __init__(self, start, dirnx=0, dirny=0, color= (255,0)):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+
+class Snake(object):
+    def __init__(self, color, pos):
+        pass
+
+    def move(self):
+        pass
+
+    def reset(self, pos):
+        pass
+
+    def add_cube(self):
+        pass
+
+    def draw(self, surface):
+        pass
+
