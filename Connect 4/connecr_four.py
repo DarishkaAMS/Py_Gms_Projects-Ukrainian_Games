59a7ce3058c18e5bf32a0e81fed3ec9466693887
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-21 15:14:29 +0200

    Add players turn

--- Connect 4/connecr_four.py
+++ Connect 4/connecr_four.py
@@ -6,6 +6,17 @@ def create_board():
 
 
 board = create_board()
+game_over = False # As no one has formed the row
+turn = 0
 
-# As no one has formed the row
-game_over = False
+while not game_over:
+    # Ask for Player 1 Input
+    if turn == 0:
+        selection = int(input("Player 1, please make your selection (0-6):"))
+    # Ask for Player 2 Input
+    else:
+        selection = int(input("Player 2, please make your selection (0-6)"))
+
+    turn += 15
+    turn %= 2
+
