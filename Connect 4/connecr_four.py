c93ba1f3092ff18028e9957eb8f590d9b0fc4572
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-21 15:22:51 +0200

    check the valid location

--- Connect 4/connecr_four.py
+++ Connect 4/connecr_four.py
@@ -5,6 +5,17 @@ def create_board():
     return np.zeros((7, 7))
 
 
+def drop_piece():
+    pass
+
+
+def is_valid_location(board, column):
+    return board[6][column] == 0
+
+
+def get_next_open_row():
+    pass
+
 board = create_board()
 game_over = False # As no one has formed the row
 turn = 0
@@ -12,11 +23,11 @@ turn = 0
 while not game_over:
     # Ask for Player 1 Input
     if turn == 0:
-        selection = int(input("Player 1, please make your selection (0-6):"))
+        column = int(input("Player 1, please make your selection (0-7):"))
     # Ask for Player 2 Input
     else:
-        selection = int(input("Player 2, please make your selection (0-6)"))
+        column = int(input("Player 2, please make your selection (0-7)"))
 
-    turn += 15
+    turn += 1
     turn %= 2
 
