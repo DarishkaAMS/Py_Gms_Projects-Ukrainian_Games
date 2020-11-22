741843af81443aeda7379a481dda79b3f13c29cc
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-22 11:31:38 +0200

    Pygame initial set-up

--- Connect 4/connecr_four.py
+++ Connect 4/connecr_four.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pygame
+import sys
 
 rows_count = 6
 columns_count = 7
@@ -50,34 +52,54 @@ def winning_move(board, piece):
         for row in range(3, rows_count):
             if board[row][column] == piece and board[row-1][column+1] == piece and board[row-2][column+2] == piece and board[row-3][column+3] == piece:
                 return True
-4
+
+
+def draw_board(board):
+    pass
+
+
 board = create_board()
 print_board(board)
 game_over = False # As no one has formed the row
 turn = 0
 
+pygame.init()
+
+square_size = 100
+square_width = columns_count * square_size
+square_height = (rows_count + 1)* square_size
+
+total_size = (square_width, square_height)
+screen = pygame.display.set_mode(total_size)
+
 while not game_over:
-    # Ask for Player 1 Input
-    if turn == 0:
-        column = int(input("Player 1, please make your selection (0-7):"))
 
-        if is_valid_location(board, column):
-            row = get_next_open_row(board, column)
-            drop_piece(board, row, column, 1)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('')
+            # Ask for Player 1 Input
+            # if turn == 0:
+            #     column = int(input("Player 1, please make your selection (0-7):"))
+            #
+            #     if is_valid_location(board, column):
+            #         row = get_next_open_row(board, column)
+            #         drop_piece(board, row, column, 1)
+            #
+            #         if winning_move(board, 1):
+            #             print("Player 1 has won!!! Congratulations!!!")
+            #             game_over = True
+            #
+            # # Ask for Player 2 Input
+            # else:
+            #     column = int(input("Player 2, please make your selection (0-7):"))
+            #
+            #     if is_valid_location(board, column):
+            #         row = get_next_open_row(board, column)
+            #         drop_piece(board, row, column, 2)
 
-            if winning_move(board, 1):
-                print("Player 1 has won!!! Congratulations!!!")
-                game_over = True
-
-    # Ask for Player 2 Input
-    else:
-        column = int(input("Player 2, please make your selection (0-7):"))
-
-        if is_valid_location(board, column):
-            row = get_next_open_row(board, column)
-            drop_piece(board, row, column, 2)
-
-    print_board(board)
-    turn += 1
-    turn %= 2
+            # print_board(board)
+            # turn += 1
+            # turn %= 2
 
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # cautious-octo-dollop
 
-Pong - blue and yellow pong for beginners 
+Pong - blue and yellow pong game for beginners 
 Snake - classical Kyiv snake game 
-Connect 4 - 
+Connect 4 - pampushka and galushka game
   
