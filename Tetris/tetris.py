aaecbf975e2c5a5abc95c62281f887e885be267f
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-24 06:39:11 +0200

    Add main

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -172,7 +172,7 @@ def check_lost(positions):
 
 
 def get_shape():
-    return random.choice(shapes)
+    return Piece(5, 0, random.choice(shapes))
 
 
 def draw_text_middle(text, size, color, surface):
@@ -206,12 +206,36 @@ def draw_window(surface, grid):
 
     surface.blit(label, (top_left_x + play_width/2 - play_width()/2, 30))
 
-    draw_grid()
+    draw_grid(surface, grid)
     pygame.display.update()
 
 
 def main():
-    pass
+    locked_position = {}
+    grid = create_grid(locked_position)
+
+    change_piece = False
+    run = True
+    current_piece = get_shape()
+    next_piece = get_shape()
+    clock = pygame.time.Clock()
+    full_time = 0
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    current_piece.x -= 1
+                if event.key == pygame.K_RIGHT:
+                    current_piece.x += 1
+                if event.key == pygame.K_DOWN:
+                    current_piece.y += 1
+                if event.key == pygame.K_UP:
+                    current_piece.rotation += 1
+
 
 
 def main_menu():
