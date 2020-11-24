2d038e1bcc50cc85ac0a59026f5ffb1fefddef02
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-24 06:30:41 +0200

    Add draw_window

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -180,14 +180,6 @@ def draw_text_middle(text, size, color, surface):
 
 
 def draw_grid(surface, grid):
-    surface.fill((0, 0, 0))
-
-    pygame.font.init()
-    game_font = pygame.font.SysFont('comicans', 60)
-    label = font.render("Tetris", 1, (255, 255, 255))
-
-    surface.blit(label, (top_left_x + play_width/2 - play_width()/2, 30))
-
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             pygame.draw.rect(surface, grid[i][j], (top_left_x + j * block_size, top_left_y + i * block_size),
@@ -205,8 +197,17 @@ def draw_next_shape(shape, surface):
     pass
 
 
-def draw_window(surface):
-    pass
+def draw_window(surface, grid):
+    surface.fill((0, 0, 0))
+
+    pygame.font.init()
+    game_font = pygame.font.SysFont('comicans', 60)
+    label = font.render("Tetris", 1, (255, 255, 255))
+
+    surface.blit(label, (top_left_x + play_width/2 - play_width()/2, 30))
+
+    draw_grid()
+    pygame.display.update()
 
 
 def main():
