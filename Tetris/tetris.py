21c5431e38d58578c4bfdfe8344966585633d0be
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-26 07:58:23 +0200

    Fixing Errors

--- Tetris/tetris.py
+++ Tetris/tetris.py
@@ -380,13 +380,13 @@ def main_menu(win):
     run = True
     while run:
         win.fill((0, 0, 0))
-        draw_text_middle('Press any key to Play', 60, (255, 255, 255))
+        draw_text_middle(win, 'Press any key to Play', 60, (255, 255, 255))
         pygame.display.update()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.KEYDOWN:
-                main()
+                main(win)
 
     pygame.display.quit()
 
