5f4540cd1ce43f169d0c5b268763028faddb46bc
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-27 06:39:06 +0200

    Add redraw_window

--- Chess/client.py
+++ Chess/client.py
@@ -6,3 +6,8 @@ win = pygame.display.set_mode(width, height)
 pygame.display.set_caption("Client")
 
 client_number = 0
+
+
+def redraw_window():
+    win.fill((0, 191, 255))
+    pygame.display.update()
