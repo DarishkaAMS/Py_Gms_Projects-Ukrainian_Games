01f835aadb5e2857bae111e4371deed760e6fb1c
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-27 08:14:00 +0200

    Fixing Errors

--- Chess/network.py
+++ Chess/network.py
@@ -3,7 +3,7 @@ import socket
 
 class Network:
     def __init__(self):
-        self.client = socket.socket(socket.IF_INET, socket.SOCK_STREAM)
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server = '192.168.1.97'
         self.port = 5555
         self.addr = (self.server, self.port)
@@ -18,3 +18,4 @@ class Network:
             pass
 
 
+netw = Network()
