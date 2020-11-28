0a702f21fecfa9c85a9baa1bd46d863ac3524d22
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2020-11-28 08:00:01 +0200

    Add read_pos and make_pos from server side

--- Chess/server.py
+++ Chess/server.py
@@ -16,8 +16,19 @@ except socket.error as error:
 sock.listen(2)  # open the post for 2 people and listen
 print("I am waiting for the connection. Server has started")
 
+pos = [(0, 0), (100, 100)]  # hold positions of the players
 
-def thread_client(conn):
+
+def read_pos(str):
+    str = str.split(',')
+    return int(str[0], int(str[1]))
+
+
+def make_pos(tup):
+    return str(tup[0] + ',' + str[1])
+
+
+def thread_client(conn, current_player):
     conn.send(str.encode('I have connected'))
     reply = ''
     while True:
@@ -40,13 +51,12 @@ def thread_client(conn):
     conn.close()
 
 
+current_player = 0
+
 while True:
     conn, addr = sock.accept()  # Accept the connection
     print(f'I have connected to: {addr}')
 
-    start_new_thread(thread_client, (conn, ))
-
-
-
-
+    start_new_thread(thread_client, (conn, current_player))
+    current_player += 1  # Keep tracking which player we are using
 
